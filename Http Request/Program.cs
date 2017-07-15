07b042fb53429b5f1c9f6ba6ffcd37a92b9c929c
Thanh Ho <wowueh@gmail.com>
2017-07-15 12:56:14 +0700

    Add project files.

---
+++ Http Request/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Http_Request
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hay nhap dia chi web: ");
+            var diaChiWeb = Console.ReadLine();
+            DuyetWebAsync(diaChiWeb);
+            Console.WriteLine("Vui long cho ...");
+            Console.ReadKey();
+        }
+        static async void DuyetWebAsync(string diaChiWeb)
+        {
+            var client = new HttpClient();
+            var response = new HttpResponseMessage();
+            response = await client.GetAsync(diaChiWeb);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("\n" + responseContent);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Request Video File/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+
+namespace Request_A_File
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hay nhap url cua File: ");
+            var diaChiFile = Console.ReadLine();
+            RequestFileAsync(diaChiFile);
+            Console.WriteLine("Vui long cho ...");
+            Console.ReadKey();
+        }
+        static async void RequestFileAsync(string diaChiFile)
+        {
+            var client = new HttpClient();
+            var response = new HttpResponseMessage();
+            response = await client.GetAsync(diaChiFile);
+            var responseContent = await response.Content.ReadAsByteArrayAsync();
+            SaveFile(responseContent);
+        }
+        static void SaveFile(byte[] bytearray)
+        {
+            var file = new FileStream("d:/file", FileMode.OpenOrCreate);
+            var bw = new BinaryWriter(file);
+            bw.Write(bytearray);
+            bw.Flush();
+            bw.Close();
+            Console.WriteLine("Da luu file thanh cong!");
+            Console.ReadKey();
+        }
+    }
+}
