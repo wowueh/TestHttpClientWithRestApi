62c3425b5158aef901c308622246d4f92ea781c9
Thanh Ho <wowueh@gmail.com>
2017-07-15 14:12:44 +0700

    Delete some unnessesary files

--- Request Video File/Properties/AssemblyInfo.cs
+++ Request Video File/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Request Video File")]
+[assembly: AssemblyTitle("Request a File")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Request Video File")]
+[assembly: AssemblyCompany("wowueh")]
+[assembly: AssemblyProduct("Request a File")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
